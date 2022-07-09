bbda1d744a54aa77b919edfc22c83441056c7c06
fab4x <fabian199429@gmail.com>
2022-07-09 21:52:07 +0200

    Update vendingscript.py
    v2

--- vendingscript.py
+++ vendingscript.py
@@ -4,14 +4,16 @@
 import time
 
 #array with products - extend as you please
-fruits = ['1. Snickers', '2. Mars', '3. Doritos']
+snacks = ['1. Snickers', '2. Mars', '3. Doritos']
+#array with prices 
+prices = ['5']
 
 while True:
     try:
         start = int(time.time())
 
         print("Please select one of the Snacks listed below\n")
-        print("\n".join(fruits))
+        print("\n".join(snacks))
 
         choice = int(input())
         cfruit = choice - 1
@@ -22,9 +24,10 @@ while True:
         print(end - start)
         print("Please wait until the machine is ready again!")
         time.sleep(10)
-
-        pass
+        
+    #if input is not a int value - this error will be printed
     except ValueError:
         print("Please enter a valid number!\n")
+    #if input int value is higher than a index number in array - this error will be printed
     except IndexError:
         print("This number is not included in the list\n")
