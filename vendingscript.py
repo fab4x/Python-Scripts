52313230580a1664bcd171cd01d1e5a56e230806
fab4x <fabian199429@gmail.com>
2022-07-09 22:07:06 +0200

    Update vendingscript.py
    v4

--- vendingscript.py
+++ vendingscript.py
@@ -6,7 +6,9 @@ import time
 #array with products - extend as you please
 snacks = ['1. Snickers', '2. Mars', '3. Doritos']
 #array with prices 
-prices = ['5']
+prices = ['2.50', '2.00', '3.00']
+#array with amount in store
+storage = ['10', '5', '1']
 
 while True:
     try:
@@ -19,10 +21,13 @@ while True:
         csnack = choice - 1
 
         chose = snacks[csnack]
+        amount = storage[csnack] - 1
+        print(amount)
         print(chose + "\n")
         end = int(time.time())
-        print(end - start)
+        #print(end - start)
         print("Please wait until the machine is ready again!")
+        #wait for 10 seconds until machine is ready again
         time.sleep(10)
         
     #if input is not a int value - this error will be printed
