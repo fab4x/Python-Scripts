7c907919d66a1fbd82542d9450d942403993954f
fab4x <fabian199429@gmail.com>
2022-07-10 12:06:53 +0200

    Update vendingscript.py
    Issue - Update amount in Array and pop item out of array if count is < 1

--- vendingscript.py
+++ vendingscript.py
@@ -1,6 +1,7 @@
 #python script for a vending machine
+#it can only handle one item at the time.
 
-#import lib to pause and to measure the time between start and end
+#import library to pause and to measure the time between start and end
 import time
 
 #array with products - extend as you please
@@ -8,27 +9,53 @@ snacks = ['1. Snickers', '2. Mars', '3. Doritos']
 #array with prices 
 prices = ['2.50', '2.00', '3.00']
 #array with amount in store
-storage = ['10', '5', '1']
+storage = [10, 5, 1]
 
+def calc():
+    for i in range(len(storage)):
+        if i < 1:
+            storage.pop(i)
+            snacks.pop(csnack)
+
+
+#try and catch to avoid an unplanned exit
 while True:
     try:
+        #start of the programm
         start = int(time.time())
 
         print("Please select one of the Snacks listed below\n")
         print("\n".join(snacks))
 
         choice = int(input())
+        #calculate index position
         csnack = choice - 1
 
         chose = snacks[csnack]
+        #calculate new item count
         amount = storage[csnack] - 1
+        #set new item count in array
+        storage[csnack] = amount
+        #print array with new amount of items
+        print(storage)
+        
+        # if amount < 1 return error and pop item out of list
+        #for i in range(len(storage)):
+         #   if i < 1:
+          #     print(i)
+           #     storage.pop(i)
+
+        #call function calc
+        calc()  
+        #print the array with the amount of items left
         print(amount)
         print(chose + "\n")
+        #end of the programm
         end = int(time.time())
         #print(end - start)
         print("Please wait until the machine is ready again!")
         #wait for 10 seconds until machine is ready again
-        time.sleep(10)
+        #time.sleep(10)
         
     #if input is not a int value - this error will be printed
     except ValueError:
